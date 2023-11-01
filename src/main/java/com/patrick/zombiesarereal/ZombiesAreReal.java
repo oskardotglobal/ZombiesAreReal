2419518499d48479ebeef55afc2c0db48c7930b2
P-Gruber <gruber@duck.com>
2023-11-01 12:50:35 -0500

    fix: set version number

--- src/main/java/com/patrick/zombiesarereal/ZombiesAreReal.java
+++ src/main/java/com/patrick/zombiesarereal/ZombiesAreReal.java
@@ -10,7 +10,7 @@ public class ZombiesAreReal
 {
     public static final String MODID   = "zombiesarereal";
     public static final String NAME    = "Zombies Are Real";
-    public static final String VERSION = "1.9.1";
+    public static final String VERSION = "1.9.2";
 
     public static final boolean DEBBUGING = false;
 
