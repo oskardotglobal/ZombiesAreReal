e9e903858a893c6d665558b42cf94950ba417278
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2024-02-18 12:34:06 +0100

    fix: clean up mixins

--- src/main/java/com/patrick/zombiesarereal/mixin/EntityPlayerMixin.java
+++ src/main/java/com/patrick/zombiesarereal/mixin/EntityPlayerMixin.java
@@ -1,14 +1,25 @@
 package com.patrick.zombiesarereal.mixin;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+@SideOnly(Side.CLIENT)
 @Mixin(EntityPlayer.class)
-public abstract class EntityPlayerMixin {
-    @Inject(at = @At("TAIL"), method = "getAlwaysRenderNameTagForRender", cancellable = true)
+public abstract class EntityPlayerMixin extends EntityLivingBase {
+    public EntityPlayerMixin(World worldIn) {
+        super(worldIn);
+    }
+
+    @Unique
+    @Inject(at = @At("TAIL"), method = "getAlwaysRenderNameTagForRender", cancellable = true, remap = false)
     private void zombiesarereal$getAlwaysRenderNameTagForRender(CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(false);
     }
--- src/main/java/com/patrick/zombiesarereal/mixin/EntityZombieMixin.java
+++ src/main/java/com/patrick/zombiesarereal/mixin/EntityZombieMixin.java
@@ -2,16 +2,22 @@ package com.patrick.zombiesarereal.mixin;
 
 
 import com.patrick.zombiesarereal.entities.CustomBaseZombie;
+import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityZombie.class)
-public abstract class EntityZombieMixin {
-    @Inject(at = @At("TAIL"), method = "<init>")
+public abstract class EntityZombieMixin extends EntityMob {
+    public EntityZombieMixin(World worldIn) {
+        super(worldIn);
+    }
+
+    @Inject(at = @At("TAIL"), method = "<init>", remap = false)
     private void init(CallbackInfo info) {
-        ((EntityZombie)(Object)this).setHealth(CustomBaseZombie.MAX_HEALTH);
+        this.setHealth(CustomBaseZombie.MAX_HEALTH);
     }
 }
